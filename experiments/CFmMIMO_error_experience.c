d378e4daa62d13017ad37590dcdf18e4848d38c2
augustus <augustus.for.official@gmail.com>
2026-06-24 19:46:21 +0900

    Q[i][j]に埋め込む形に変更

--- experiments/CFmMIMO_error_experience.c
+++ experiments/CFmMIMO_error_experience.c
@@ -17,6 +17,7 @@ typedef struct optimal_solution{
     double opt_idx;
 } Opt_sol;
 
+// 座標間の距離を与える関数
 double distance(struct coordinates a, struct coordinates b){
     double sub_x = a.x - b.x;
     double sub_y = a.y - b.y;
@@ -28,11 +29,27 @@ double capacity(double d, double alpha, double beta){
     return -10 * alpha * log10(d) - beta;
 };
 
-// 実際の係数
+// 実際の係数を計算する関数
 double make_g(double d, double alpha, double beta, double pow_sigma){
     // 1000で除すのは、dBm => dB　の変換か？
     return pow((double) 10.0, capacity(d,alpha,beta) / 10.0 / pow_sigma / 1000);
 };
+
+// ペナルティ項を展開して与える関数
+double penalty_coef(int i, int j, double RH_const, double *coef, double hyper_parameter){
+    // 一次項
+    if( i == j ){
+        return (coef[i] * coef[i] - 2 * RH_const * coef[i]) * hyper_parameter;
+    }
+
+    // 二次項
+    return 2 * coef[i] * coef[j] * hyper_parameter;
+}
+
+// ペナルティ項の定数部分
+double const_penalty(double RH_const, double hyper_parameter){
+    return hyper_parameter * RH_const * RH_const;
+}
 
 // (0,0) ~ (max_x, max_y) のランダム Coordinates 型を返却
 Coordinates gene_random_coordinate(double maxs[2]){
@@ -52,11 +69,12 @@ Coordinates gene_random_coordinate(double maxs[2]){
     return c;
 };
 
-// H0 の部分.
-double obj_function(int i, int j ,double **g, int num_Users, int num_APs){
+// H0 を Q[i][j] に埋め込み
+void obj_function(double **Q ,double **g, int num_Users, int num_APs){
     int range_explanatory_var = num_Users * num_APs;    //スラック変数でなく、説明変数部分の範囲
-    if ((i < range_explanatory_var) && (j < range_explanatory_var)){
-         /* i,jから各々のユーザー番号, AP番号を逆算*/
+    for (int i=0; i < range_explanatory_var; i++){
+        for ( int j=0; j < range_explanatory_var; j++){
+            /* i,jから各々のユーザー番号, AP番号を逆算*/
             int index_user_i = i / num_APs;
             int index_user_j = j / num_APs;
             int index_AP_i = i % num_APs;
@@ -64,58 +82,67 @@ double obj_function(int i, int j ,double **g, int num_Users, int num_APs){
             if (i == j) //上参画ではなく対象行列になることに注意
             {
                 // 一次項 は g 1個
-                return -1 * g[index_user_i][index_AP_i];    
+                Q[i][j] += -1 * g[index_user_i][index_AP_i];    
             }
             else if (index_user_i != index_user_j)
             {
                 // 二次項は g * g
-                return -1 * g[index_user_i][index_AP_i] * g[index_user_j][index_AP_j];
-            }else {
-                return 0;   // 同一ユーザの変数の積の係数は 0
+                Q[i][j] += -1 * g[index_user_i][index_AP_i] * g[index_user_j][index_AP_j];
             }
-    } 
-
-    return 0;   // i,jのどちらかがスラック変数の場合も 0
+        }
+    }
 };
 
-// <= sup_AP の AP制約項
-double sub_AP(int i, int j, int num_Users, int num_APs, int sup_AP){
+// <= sup_AP の AP制約項. return 値は const
+double sub_AP(double *Q , int num_Users, int num_APs, int sup_AP, double hyper_param){
+    double term_const = 0.0;
     int range_explanatory_var = num_Users * num_APs;    //スラック変数でなく、説明変数部分の範囲
     int num_slack_AP = ((int)(log(sup_AP) / log(2)) + 1);   // AP制約に必要なスラック変数の数
+    int index_start_slack = num_Users * num_APs;    // スラック変数の開始位置
 
-    // スラック変数に関係なかったら return 0
-    if ( ((i < range_explanatory_var) || (range_explanatory_var + num_APs <= i))
-        &&((j < range_explanatory_var) || (range_explanatory_var + num_APs <= j))){
-            return 0;
+    for (int index_AP=0;index_AP<num_APs;index_AP++){
+        double coef_pen[range_explanatory_var + num_slack_AP];  // 係数を記録
+        Initialization_array_double(coef_pen, range_explanatory_var + num_slack_AP);
+    
+        for(int j=0; j<num_Users; j++){
+            coef_pen[index_AP + j * num_APs] = 1.0;
         }
+        for(int j=0; j<num_slack_AP; j++){
+            // []内は, start位置 + index_AP個目までののAPについての制約で使ったスラック変数の個数 + 今の制約項での j 個目
+            coef_pen[index_start_slack + index_AP * num_slack_AP +  j] = -1 * pow(2.0, (double) j);
+        }
+        term_const += embed_pow_in_Qij(Q, sup_AP, coef_pen, hyper_param);
+    }
     
-    // i,jのどちらかがスラック変数に関係があれば
-    
+    return term_const;
 };
 
 // L <= の User制約項
-double sub_User(int i, int j, int num_Users, int num_APs, int inf_User, int sup_AP){
+double sub_User(double *Q , int num_Users, int num_APs, int inf_User, int sup_AP, double hyper_param){
     // AP制約とUser制約両方のスラック変数の数をカウントする
     // そうしないと　User制約のスラック変数が何番目から始まるのかがわからない
     int num_slack_AP = ((int)(log(sup_AP) / log(2)) + 1);
     int num_slack_Users = ((int) (log(num_APs - inf_User) / log(2)) + 1);
+
+    
 };
 
 // インデックス番号 [i][j]　にて　Q[i][j] の要素を return する
 // sup_AP : 各APの接続上限
 // inf_User : 各Userの接続下限
-double make_QUBO_coef(int i, int j, double **g, int num_Users, int num_APs, int sup_AP, int inf_User){
+double make_QUBO_coef(double **Q, double **g, int num_Users, int num_APs, int sup_AP, int inf_User, double hyper_param){
+    double term_const = 0.0;
+
     // 目的関数部分
-    double H0 = obj_function(i, j, g, num_Users, num_APs);
+    obj_function(Q, g, num_Users, num_APs);
 
     // 制約項1 : 各APの上限制約
-    double H1 = sub_AP(i, j, num_Users, num_Users, sup_AP);
+    term_const += sub_AP(Q, num_Users, num_Users, sup_AP, hyper_param);
 
     // 制約項2 : 各Userの下限制約
-    double H2 = sub_User(i, j, num_APs,num_Users, inf_User, sup_AP);
+    term_const += sub_User(Q , num_APs,num_Users, inf_User, sup_AP, hyper_param);
 
-    // すべてを足し合わせて返却する
-    return H0 + H1 + H2;
+    return term_const;
 };
 
 int main(){
@@ -149,7 +176,7 @@ int main(){
         
         for(int index_Users=0; index_Users < num_Users; index_Users++){
             // 乱数によるUser生成
-            location_Users[index_Users] = gene_random_coordinate([width_x,width_y]);
+            location_Users[index_Users] = gene_random_coordinate([width_x, width_y]);
 
             // AP,User座標から係数行列 g[num_Users][num_APs]を作成
             for(int index_APs = 0; index_APs < num_APs; index_APs++){
